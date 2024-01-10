4b320f15eae5c6a4c97b9a5e87b957876ed9a2f4
Petro <petrosahal66@gmail.com>
2024-01-10 13:44:45 +0200

    readi timer

--- js/script.js
+++ js/script.js
@@ -1,2 +1,61 @@
 'use strict';
+const newYear = '2024-12-31';
 
+    function getTimeRemaining(endTime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
+              
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'second': seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              second = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updataClock, 1000);
+        
+        updataClock();      
+
+        function updataClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            second.innerHTML = getZero(t.second);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', newYear)
